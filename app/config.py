d96f79e9e22dcea2e5e3f6a3b40d38e9c0c49198
RetroJimmyX <51858595+RetroJimmyX@users.noreply.github.com>
2026-08-22 05:34:11 +0100

    Store GPTWOL schedules in native data directory

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 DATA_DIR = os.environ.get('GPTWOL_DATA_DIR', '/app/db')
-SCHEDULE_FILE = os.environ.get('GPTWOL_SCHEDULE_FILE', '/etc/cron.d/gptwol')
+SCHEDULE_FILE = os.environ.get('GPTWOL_SCHEDULE_FILE', os.path.join(DATA_DIR, 'gptwol.cron'))
 LEGACY_COMPUTERS_FILE = os.path.join(DATA_DIR, 'computers.txt')
 DATABASE_FILE = os.path.join(DATA_DIR, 'computers.db')
 
